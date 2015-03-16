c4e025a908c63421dff3860039e96da626085293
samiulh <samiul.x.hasan@gsk.com>
2015-03-16 17:06:49 +0000

    added disease tests

---
+++ test/disease/run.bat
@@ -0,0 +1,3 @@
+z-schema ..\..\src\bioentity\disease.json instance_success.json
+z-schema ..\..\src\bioentity\disease.json instance_fail.json
+z-schema ..\..\src\bioentity\disease.json instance_fail1.json
