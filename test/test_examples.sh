f9506646f05d70389c853734e5825c135ecd8f1a
samiulh <samiul.x.hasan@gsk.com>
2015-04-13 16:10:22 +0100

    Added known drugs example

--- test/test_examples.sh
+++ test/test_examples.sh
@@ -1,2 +1,3 @@
 z-schema ../src/target_disease_association.json ../examples/expression.json
 z-schema ../src/target_disease_association.json ../examples/gwas.json
+z-schema ../src/target_disease_association.json ../examples/known_drug.json
