324cc7a73b58988d8d18c33086bef553c2abdfde
samiulh <samiul.x.hasan@gsk.com>
2015-04-21 14:31:50 +0100

    fixed url in evidence-literature

--- test/test_examples.sh
+++ test/test_examples.sh
@@ -1,4 +1,4 @@
-z-schema ../src/target_disease_association.json ../examples/expression.json
-z-schema ../src/target_disease_association.json ../examples/gwas.json
-z-schema ../src/target_disease_association.json ../examples/known_drug.json
-z-schema ../src/target_disease_association.json ../examples/generic.json
+z-schema ../src/expression.json ../examples/expression.json
+z-schema ../src/genetics.json ../examples/gwas.json
+z-schema ../src/drug.json ../examples/known_drug.json
+z-schema ../src/literature.json ../examples/literature.json
