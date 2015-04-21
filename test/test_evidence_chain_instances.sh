c6b808a6c7e9fcaa693db14c8fd46c563c7f0fc0
samiulh <samiul.x.hasan@gsk.com>
2015-04-21 13:58:00 +0100

    removed temp examples

--- test/test_evidence_chain_instances.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./test_z-schema.sh &&
-
-echo -e "TESTSUITE: EVIDENCE_CHAIN\n"
-
-z-schema ../src/evidence_chain/generic.json ./evidence_chain/generic.json
--- test/test_td_associations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./test_z-schema.sh &&
-
-echo -e "TESTSUITE: TARGET-DISEASE-ASSOCIATION OBJECTS\n"
-
-z-schema ../src/target_disease_association.json ./target_disease_assoc/target_disease_association.json
