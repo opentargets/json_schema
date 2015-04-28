3b756d0d5ea58904ba7d42e6e941ac3ee42cf77d
samiulh <samiul.x.hasan@gsk.com>
2015-04-28 18:29:18 +0100

    updated examples

--- test/test_examples.sh
+++ test/test_examples.sh
@@ -1,4 +1,4 @@
 z-schema ../src/expression.json ../examples/expression.json
 z-schema ../src/genetics.json ../examples/gwas.json
 z-schema ../src/drug.json ../examples/known_drug.json
-z-schema ../src/literature.json ../examples/literature.json
+z-schema ../src/literature_mining.json ../examples/literature_mining.json
