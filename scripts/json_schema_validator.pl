9d0dc52ed5fa8be3d58c990bfbcabfca1e53fe98
Wojtek Bazant <wbazant@ebi.ac.uk>
2017-02-02 14:59:20 +0000

    Print line numbers when validating. Print error messages to stderr, and fail with nonzero exit code when errors found

--- scripts/json_schema_validator.pl
+++ scripts/json_schema_validator.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use JSON;
 use Data::Dumper;
@@ -9,8 +9,8 @@ my $schema = $ARGV[1];
 
 my $validator = JSON::Validator->new;
 $validator->schema($schema);
-my $count = 0;
-
+my $line_count = 1;
+my $err_count = 0;
 open(FILE, "<$json");
 
 while(<FILE>){
@@ -20,11 +20,12 @@ while(<FILE>){
    my @errors = $validator->validate(from_json($line));
 
    if(@errors){
-      print Dumper $_ foreach @errors;
-    }
-   else {
-     $count++;
+      warn "Error on line $line_count : \t";
+      warn Dumper $_ foreach @errors;
+      $err_count++;	
    }
+   $line_count++;
 }
-
-print "$count evidence(s) PASSED schema validation !\n" if($count >0);
+if($err_count>0){
+   die "Exited with $err_count errors"
+}
