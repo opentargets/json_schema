94b6f62b478d18f601e33113ea40ae5517ab5e73
wbazant <wbazant@ebi.ac.uk>
2017-05-15 12:32:42 +0100

    Make the validation script more easy to use - validate that a file was provided and provide a default schema. Document how to use it. Make the script more chatty to facilitate keeping track of its progress (with \r so it only modifies the current terminal line)

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-#CTTV JSON schema 1.2.3
+#CTTV JSON schema
 
-#### ![info](https://github.com/CTTV/input_data_format/raw/master/help/images/info.gif) [**Instructions for generating  target-disease-association objects using schema 1.2.3**](doc/instructions.md)
+#### ![info](https://github.com/CTTV/input_data_format/raw/master/help/images/info.gif) [**Instructions for generating  target-disease-association objects using newest schema**](doc/instructions.md)
 
 ## Schema overview
 
 ![Schema 1.2 overview](imgs/schema_1.2.png)
+
+
+## Validation
+Execute something like:
+```
+git clone https://github.com/opentargets/json_schema.git /var/tmp/json_schema && /var/tmp/json_schema/scripts/json_schema_validator.pl <your file>
+```
--- scripts/json_schema_validator.pl
+++ scripts/json_schema_validator.pl
@@ -3,9 +3,15 @@ use strict;
 use JSON;
 use Data::Dumper;
 use JSON::Validator;
+use File::Basename;
 
+if ($#ARGV < 0) {
+    print "\nUsage: json_schema_validator.pl <file_to_validate> (<schema>)";
+    exit;
+}
 my $json   = $ARGV[0];
-my $schema = $ARGV[1];
+my $dir = dirname(__FILE__);
+my $schema = $ARGV[1] // "$dir/../src/base.json" ;
 
 my $validator = JSON::Validator->new;
 $validator->schema($schema);
@@ -20,18 +26,24 @@ while(<FILE>){
    chomp($line);
 
    my @errors = $validator->validate(from_json($line));
+   $line_count++;
 
    if(@errors){
       print "Validation error on evidence line $line_count : \t";
       print Dumper $_ foreach @errors;
-      $err_count++;	
+      $err_count++;
+   } else {
+     print "\r Successfully validated line $line_count ..."
+
    }
-   $line_count++;
+   if($err_count > 1000){
+     die "Too many errors. Giving up"
+   }
 }
 
 if($err_count>0){
    die "Exited with $err_count evidence line(s) with errors of the total $line_count\n";
 }
 else {
-   print "$line_count evidence PASSED schema validation!\n";
+   print "\n$line_count evidence PASSED schema validation!\n";
 }
